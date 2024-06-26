a4fb3315814af7dc0cfa49f6ff272bed627e50eb
k-barseghyan-pcln <karen.barseghyan@priceline.com>
2024-06-26 12:37:13 +0400

    Test;

--- locks/src/LockApplication.java
+++ locks/src/LockApplication.java
@@ -1,13 +1,20 @@
 import org.karen.lock.NonReentrantLock;
+import org.karen.lock.ReentrantLock;
 import org.karen.lock.TASLock;
 
 public class LockApplication {
 
     public static void main(String[] args) {
+
+        TASLock lock2 = new ReentrantLock();
+        lock2.lockTAS();
+        lock2.lockTAS();
+        System.out.println("Reentered!");
+
         TASLock lock = new NonReentrantLock();
         lock.lockTAS();
-        System.out.println("Locked");
-        lock.unlock();
-        System.out.println("Unlocked");
+        System.out.println("Locked non-reentrant");
+        lock.lockTAS();
+        System.out.println("Buggy non-reentrant");
     }
 }
